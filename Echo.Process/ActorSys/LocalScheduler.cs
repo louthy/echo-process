louthy/echo-process

commit 971fde0c02ff2527bc401e2ed74b7d093599dcbf

Replace Map with HashMap

Echo.Process/ActorSys/LocalScheduler.cs
@@ -12,8 +12,8 @@ namespace Echo
     internal static class LocalScheduler
     {
         static object sync = new object();
-        static Map<long, Seq<(string key, Func<object, Unit> action, object message, ActorRequestContext context, Option<SessionId> sessionId)>> actions;
-        static Map<string, long> keys;
+        static HashMap<long, Seq<(string key, Func<object, Unit> action, object message, ActorRequestContext context, Option<SessionId> sessionId)>> actions;
+        static HashMap<string, long> keys;
         static Que<(Schedule schedule, ProcessId pid, Func<object, Unit> action, object message, ActorRequestContext context, Option<SessionId> sessionId)> inbound;
 
         public static Unit Push(Schedule schedule, ProcessId pid, Func<object, Unit> action, object message)

Echo.Process.Redis/RedisClusterImpl.cs
@@ -22,7 +22,7 @@ namespace Echo
         int databaseNumber;
         ConnectionMultiplexer redis;
 
-        Map<string, Subject<RedisValue>> subscriptions = Map<string, Subject<RedisValue>>();
+        HashMap<string, Subject<RedisValue>> subscriptions = HashMap<string, Subject<RedisValue>>();
         object subsync = new object();
 
         /// <summary>
@@ -298,7 +298,7 @@ namespace Echo
             Retry(() =>
                 Db.HashSet(key, field, JsonConvert.SerializeObject(value)));
 
-        public void HashFieldAddOrUpdate<T>(string key, Map<string, T> fields) =>
+        public void HashFieldAddOrUpdate<T>(string key, HashMap<string, T> fields) =>
             Retry(() =>
                 Db.HashSet(
                     key, 
@@ -314,7 +314,7 @@ namespace Echo
                 return trans.Execute();
             });
 
-        public bool HashFieldAddOrUpdateIfKeyExists<T>(string key, Map<string, T> fields) =>
+        public bool HashFieldAddOrUpdateIfKeyExists<T>(string key, HashMap<string, T> fields) =>
             Retry(() =>
             {
                 var trans = Db.CreateTransaction();
@@ -335,19 +335,19 @@ namespace Echo
             Retry(() =>
                 (int)Db.HashDelete(key, fields.Map(x => (RedisValue)x).ToArray()));
 
-        public Map<string, object> GetHashFields(string key) =>
+        public HashMap<string, object> GetHashFields(string key) =>
             Retry(() =>
                 Db.HashGetAll(key)
                   .Fold(
-                    Map<string, object>(),
+                    HashMap<string, object>(),
                     (m, e) => m.Add(e.Name, JsonConvert.DeserializeObject(e.Value)))
                   .Filter((object v) => notnull(v)));
 
-        public Map<string, T> GetHashFields<T>(string key) =>
+        public HashMap<string, T> GetHashFields<T>(string key) =>
             Retry(() =>
                 Db.HashGetAll(key)
                   .Fold(
-                    Map<string, T>(),
+                    HashMap<string, T>(),
                     (m, e) => m.Add(e.Name, JsonConvert.DeserializeObject<T>(e.Value)))
                   .Filter(v => notnull<T>(v)));
 
@@ -376,11 +376,11 @@ namespace Echo
             }
         }
 
-        public Map<K, T> GetHashFields<K, T>(string key, Func<string,K> keyBuilder) =>
+        public HashMap<K, T> GetHashFields<K, T>(string key, Func<string,K> keyBuilder) =>
             Retry(() =>
                 Db.HashGetAll(key)
                   .Fold(
-                    Map<K, T>(),
+                    HashMap<K, T>(),
                     (m, e) => m.Add(keyBuilder(e.Name), JsonConvert.DeserializeObject<T>(e.Value)))
                   .Filter(v => notnull<T>(v)));
 
@@ -391,13 +391,13 @@ namespace Echo
             return JsonConvert.DeserializeObject<T>(res);
         }
 
-        public Map<string, T> GetHashFields<T>(string key, IEnumerable<string> fields) =>
+        public HashMap<string, T> GetHashFields<T>(string key, IEnumerable<string> fields) =>
             Retry(() =>
                 Db.HashGet(key, fields.Map(x => (RedisValue)x).ToArray())
                   .Zip(fields)
                   .Filter(x => !x.Item1.IsNullOrEmpty)
                   .Fold(
-                      Map<string, T>(),
+                      HashMap<string, T>(),
                       (m, e) => m.Add(e.Item2, JsonConvert.DeserializeObject<T>(e.Item1)))
                   .Filter(v => notnull<T>(v)));
 
@@ -465,8 +465,8 @@ namespace Echo
         /// </summary>
         /// <param name="keyQuery">Key query.  * is a wildcard</param>
         /// <returns>Map of ProcessId to ProcessMetaData</returns>
-        public Map<ProcessId, ProcessMetaData> QueryProcessMetaData(string keyQuery) =>
-            toMap(
+        public HashMap<ProcessId, ProcessMetaData> QueryProcessMetaData(string keyQuery) =>
+            toHashMap(
                 map(QueryKeys(keyQuery, "", metaDataSuffix).Map(x => (RedisKey)x).ToArray(), keys =>
                     keys.Map(x => (string)x)
                         .Map( x => (ProcessId)x.Substring(0 ,x.Length - metaDataSuffix.Length))
@@ -477,18 +477,18 @@ namespace Echo
         /// </summary>
         /// <param name="keys"></param>
         /// <returns>map of keys and their key/value map</returns>
-        public async Task<Map<string, Map<string, object>>> GetAllHashFieldsInBatch(Seq<string> keys)
+        public async Task<HashMap<string, HashMap<string, object>>> GetAllHashFieldsInBatch(Seq<string> keys)
         {
             var batch = Db.CreateBatch();
             var tasks = keys.Map(key => batch.HashGetAllAsync(key)
                                              .Map(h =>
-                                                (Key: key, Value: toMap(h.Map(r =>
+                                                (Key: key, Value: toHashMap(h.Map(r =>
                                                     ((string)r.Name, JsonConvert.DeserializeObject(r.Value)))))))
                             .Strict();
 
             batch.Execute();
 
-            return toMap(await Task.WhenAll(tasks));
+            return toHashMap(await Task.WhenAll(tasks));
         }
 
         IDatabase Db => 

Echo.Process/ActorSys/Actor.cs
@@ -28,8 +28,8 @@ namespace Echo
         readonly Subject<object> stateSubject = new Subject<object>();
         readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         readonly Option<ICluster> cluster;
-        Map<string, IDisposable> subs = Map<string, IDisposable>();
-        Map<string, ActorItem> children = Map<string, ActorItem>();
+        HashMap<string, IDisposable> subs = HashMap<string, IDisposable>();
+        HashMap<string, ActorItem> children = HashMap<string, ActorItem>();
         Option<S> state;
         StrategyState strategyState = StrategyState.Empty;
         EventWaitHandle request;
@@ -180,7 +180,7 @@ namespace Echo
         Unit RemoveAllSubscriptions()
         {
             subs.Iter(x => x.Dispose());
-            subs = Map<string, IDisposable>();
+            subs = HashMap<string, IDisposable>();
             return unit;
         }
 
@@ -343,7 +343,7 @@ namespace Echo
         /// <summary>
         /// Child processes
         /// </summary>
-        public Map<string, ActorItem> Children =>
+        public HashMap<string, ActorItem> Children =>
             children;
 
         public CancellationTokenSource CancellationTokenSource => cancellationTokenSource;
@@ -1014,7 +1014,7 @@ namespace Echo
                 {
                     ShutdownProcessRec(self, sys.GetInboxShutdownItem().Map(x => (ILocalActorInbox)x.Inbox), maintainState);
                     Parent.Actor.UnlinkChild(Id);
-                    children = Map<string, ActorItem>();
+                    children = HashMap<string, ActorItem>();
                 });
             }
         }

Echo.Process/ActorSys/ActorDispatchGroup.cs
@@ -44,10 +44,10 @@ namespace Echo
         public Unit DispatchWatch(ProcessId pid) =>
             IterRoleMembers(d => d.DispatchWatch(pid));
 
-        public Map<string, ProcessId> GetChildren() =>
+        public HashMap<string, ProcessId> GetChildren() =>
             List.fold(
                 MapRoleMembers(disp => disp.GetChildren()),
-                Map.empty<string, ProcessId>(), 
+                HashMap.empty<string, ProcessId>(), 
                 (s, x) => s + x
                 );
 

Echo.Process/ActorSys/ActorDispatchJS.cs
@@ -24,7 +24,7 @@ namespace Echo
             this.transactionalIO = transactionalIO;
         }
 
-        public Map<string, ProcessId> GetChildren()
+        public HashMap<string, ProcessId> GetChildren()
         {
             throw new NotImplementedException();
         }

Echo.Process/ActorSys/ActorDispatchLocal.cs
@@ -87,7 +87,7 @@ namespace Echo
                 () => Actor.ShutdownProcess(maintainState)
             );
 
-        public Map<string, ProcessId> GetChildren() =>
+        public HashMap<string, ProcessId> GetChildren() =>
             Actor.Children.Map(a => a.Actor.Id);
 
         public Unit Publish(object message) =>

Echo.Process/ActorSys/ActorDispatchNotExist.cs
@@ -19,8 +19,8 @@ namespace Echo
         private T Raise<T>(ProcessId sender) =>
             raise<T>(new ProcessException($"Doesn't exist ({ProcessId})", sender.Path, sender.Path, null));
 
-        public Map<string, ProcessId> GetChildren() =>
-            Map<string, ProcessId>();
+        public HashMap<string, ProcessId> GetChildren() =>
+            HashMap<string, ProcessId>();
 
         public IObservable<T> Observe<T>() =>
             Observable.Empty<T>();

Echo.Process/ActorSys/ActorDispatchRemote.cs
@@ -27,8 +27,8 @@ namespace Echo
             this.transactionalIO = transactionalIO;
         }
 
-        public Map<string, ProcessId> GetChildren() =>
-            ask<Map<string, ProcessId>>(ProcessId, UserControlMessage.GetChildren);
+        public HashMap<string, ProcessId> GetChildren() =>
+            ask<HashMap<string, ProcessId>>(ProcessId, UserControlMessage.GetChildren);
 
         public IObservable<T> Observe<T>() =>
             Cluster.SubscribeToChannel<T>(ActorInboxCommon.ClusterPubSubKey(ProcessId));

commit df3efd87383fcf72465d8a9e36ffe58e9cdae5b9

LocalScheduler does not work
This change https://github.com/louthy/echo-process/commit/971fde0c02ff2527bc401e2ed74b7d093599dcbf  broke the LocalScheduler as we rely on ordering (line 161). Changed back to `Map`.

Echo.Process/ActorSys/LocalScheduler.cs
@@ -12,7 +12,8 @@ namespace Echo
     internal static class LocalScheduler
     {
         static object sync = new object();
-        static HashMap<long, Seq<(string key, Func<object, Unit> action, object message, ActorRequestContext context, Option<SessionId> sessionId)>> actions;
+        // using Map for 'actions' as we rely on order
+        static Map<long, Seq<(string key, Func<object, Unit> action, object message, ActorRequestContext context, Option<SessionId> sessionId)>> actions;
         static HashMap<string, long> keys;
         static Que<(Schedule schedule, ProcessId pid, Func<object, Unit> action, object message, ActorRequestContext context, Option<SessionId> sessionId)> inbound;
 