62c55315b389db7b1b50f3659e1d6d8264071845
Paul Louth <plouth@meddbase.com>
2021-03-12 14:01:46 +0000

    2.1.6-beta

---
+++ Echo.Process.AspNetCore/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+namespace Echo
+{
+    public static class ProcessSysMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseProcessSys(this IApplicationBuilder builder) =>        
+            builder.UseWhen(
+                predicate: context => context.Request.Path == "/process-sys",
+                configuration: appBuilder =>
+                               {
+                                   appBuilder.UseWebSockets();
+                                   appBuilder.Use(async (context, next) =>
+                                                  {
+                                                      if (context.WebSockets.IsWebSocketRequest)
+                                                      {
+                                                          await new ProcessSysListener(context).Listen();
+                                                      }
+                                                  });
+                               });
+    }    
+}
