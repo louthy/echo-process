cd81e6e4eef1c40c98aeb184317d7cd749972a73
louthy <p.louth@gmail.com>
2023-03-01 10:16:27 +0000

    2.1.35-beta

---
+++ Echo.Process/ActorSys/PausableBlockingQueueAsync.cs
@@ -0,0 +1,125 @@
+﻿using Echo;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace Echo.ActorSys
+{
+    public class PausableBlockingQueueAsync<A> : IDisposable
+    {
+        readonly EventWaitHandle pauseWait = new AutoResetEvent(false);
+        readonly EventWaitHandle inboxComplete = new AutoResetEvent(false);
+        readonly BlockingCollection<A> items;
+        readonly CancellationTokenSource tokenSource;
+        readonly CancellationToken token;
+
+        volatile bool paused;
+        public bool IsPaused => paused;
+        public bool IsCancelled => token.IsCancellationRequested;
+
+        public PausableBlockingQueueAsync(int boundedCapacity)
+        {
+            boundedCapacity = boundedCapacity < 1 ? 100000 : boundedCapacity;
+            tokenSource = new CancellationTokenSource();
+            token = tokenSource.Token;
+            items = new BlockingCollection<A>(boundedCapacity);
+        }
+
+        public int Count =>
+            items.Count;
+
+        public IDisposable ReceiveAsync<S>(S state, Func<S, A, Task<InboxDirective>> handler) =>
+            Task.Factory.StartNew(() =>
+            {
+                bool addToFrontOfQueue = false;
+                var directive = default(InboxDirective);
+
+                if (token.IsCancellationRequested) return;
+
+                try
+                {
+                    foreach (var item in items.GetConsumingEnumerable(token))
+                    {
+                        do
+                        {
+                            if (token.IsCancellationRequested) return;
+
+                            if (IsPaused)
+                            {
+                                pauseWait?.WaitOne();
+                            }
+
+                            try
+                            {
+                                handler(state, item).ContinueWith(t =>
+                                {
+                                    directive = t.Result;
+                                    inboxComplete.Set();
+                                }, token);
+                                inboxComplete.WaitOne();
+                            }
+                            catch (Exception e)
+                            {
+                                Process.logErr(e);
+                            }
+
+                            if (directive.HasFlag(InboxDirective.Shutdown))
+                            {
+                                addToFrontOfQueue = false;
+                                Cancel();
+                            }
+                            else
+                            {
+                                addToFrontOfQueue = directive.HasFlag(InboxDirective.PushToFrontOfQueue);
+
+                                if (directive.HasFlag(InboxDirective.Pause))
+                                {
+                                    Pause();
+                                }
+                            }
+                        }
+                        while (addToFrontOfQueue);
+                    }
+                }
+                catch(OperationCanceledException)
+                {
+                    return;
+                }
+            }, token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+
+        public void Post(A value)
+        {
+            if (!items.TryAdd(value))
+            {
+                throw new QueueFullException();
+            }
+        }
+
+        public void Cancel()
+        {
+            tokenSource?.Cancel();
+            pauseWait?.Set();
+        }
+
+        public void Pause() =>
+            paused = true;
+
+        public void UnPause()
+        {
+            paused = false;
+            pauseWait?.Set();
+        }
+
+        public void Dispose()
+        {
+            Cancel();
+            tokenSource?.Dispose();
+            pauseWait?.Dispose();
+            items?.Dispose();
+            inboxComplete?.Dispose();
+        }
+    }
+}
